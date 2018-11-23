57ba43866766b4e90444ce9f958832c46f920ded
ANG WEI XUAN, DION _ <dion.ang.2016@smu.edu.sg>
2018-11-23 21:51:32 +0800

    added similarity count, but time consuming operation
    Signed-off-by: ANG WEI XUAN, DION _ <dion.ang.2016@smu.edu.sg>

--- keyframe_extractor.py
+++ keyframe_extractor.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename
 import os.path
 import requests, os
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -13,7 +14,8 @@ filename = 'video.mp4'
 video_path = os.path.join(app.config["VIDEO_UPLOAD_FOLDER"], filename)
 vidcap = cv2.VideoCapture(video_path)
 count = 0
-prev = None
+prev_image = None
+
 while True:
     # set time in video to capture
     vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) 
@@ -24,10 +26,23 @@ while True:
     # save frame as JPEG file
     cv2.imwrite(os.path.join(folder,"frame{:d}.jpg".format(count)), image)     
     
-    if not prev is None:
-        abs_diff = cv2.absdiff(image, prev)
-        total_diff = cv2.sumElems(abs_diff)
-        print(total_diff)
+    if not prev_image is None:
+        total_similarity = 0
+        row_length = len(image)
+        col_length = len(image[0])
+        for row in range(row_length):
+            for col in range(col_length):
+                rgb = image[row][col]
+                prev_rgb = prev_image[row][col]
+                dot_product = int(rgb[0])*int(prev_rgb[0]) + int(rgb[1])*int(prev_rgb[1]) + int(rgb[2])*int(prev_rgb[2])
+                magnitude_a = (rgb[0]**2 + rgb[1]**2 + rgb[2]**2) ** 0.5
+                magnitude_b = (prev_rgb[0]**2 + prev_rgb[1]**2 + prev_rgb[2]**2) ** 0.5
+                
+                cos_similarity = dot_product / (magnitude_a * magnitude_b) if magnitude_a != 0 and magnitude_b !=0 else 0
+                total_similarity += cos_similarity 
+            
+        total_similarity /= (row_length * col_length)
+        print(total_similarity)
 
-    prev = image
+    prev_image = image
     count += 1
